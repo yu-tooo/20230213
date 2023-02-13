d0cc664740132528beed11c1285e06aa913f05bc
yu-tooo <teg.moritayuto@gmail.com>
2023-02-13 13:47:08 +0900

    first commit

---
+++ 20230213_morita/resources/views/thanks.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+  <style>
+    body {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .message {
+      text-align: center;
+    }
+    .message h2 {
+      font-size: 16px;  
+    }
+    .message button {
+      cursor: pointer;
+      padding: 5px 30px;
+      font-size: 14px;
+      background-color: black;
+      color: white;
+      border-radius: 5px;
+      box-shadow: 0 0 8px gray;
+    }
+  </style>
+</head>
+<body>
+  <div class="message">
+    <h2>ご意見いただきありがとうございました。</h2>
+      <button>トップページへ</button>
+  </div>
+</body>
+</html>
